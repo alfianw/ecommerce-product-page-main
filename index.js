314c25f54ff482579ad38cd852dacf43084c6ddd
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-06-22 17:02:28 +0700

    done membuat modal

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("modals").style.display = "block"
+  }
+
+  function closeModal() {
+    document.getElementById("modals").style.display = "none"
+  }
