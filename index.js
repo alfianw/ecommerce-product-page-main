5d447888fcdc4a66d5b7deb2d7496445a30f2155
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-07-03 14:54:55 +0700

    menambahkan fiture click img

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 function myFunction() {
-    document.getElementById("modals").style.display = "block"
-  }
+  document.getElementById("modals").style.display = "block"
+}
 
-  function closeModal() {
-    document.getElementById("modals").style.display = "none"
+function closeModal() {
+  document.getElementById("modals").style.display = "none"
+}
+
+let nilai = 0;
+
+function tambahProduk() {
+  nilai += 1;
+  document.getElementById("jumlahProduk").innerText = nilai;
+}
+
+function kurangProduk() {
+  if (nilai == 0) {
+    nilai += 0
+  } else if (nilai > 0) {
+    nilai -= 1;
   }
+  document.getElementById("jumlahProduk").innerText = nilai;
+}
+
+// content
+function tampilkanFoto(namaFoto) {
+  const fotoElement = document.getElementById("imgContain")
+  fotoElement.src = namaFoto
+}
+
+// Modal
+function tampilkanFoto1(namaFoto) {
+  const fotoElement = document.getElementById("imgModal")
+  fotoElement.src = namaFoto
+}
+
+const fotoList = ["./images/image-product-1.jpg", "./images/image-product-2.jpg", "./images/image-product-3.jpg", "./images/image-product-4.jpg"];
+
+let currentIndex = 0;
+function gantiFoto1() {
+  const foto = document.getElementById("imgModal");
+  if (currentIndex <= 3) {
+    currentIndex = (currentIndex + 1) % fotoList.length;
+  }
+  foto.src = fotoList[currentIndex];
+}
+
+function gantiFoto2() {
+  const foto = document.getElementById("imgModal");
+  if (currentIndex > 0) {
+    currentIndex--
+    console.log(currentIndex)
+  }else if( currentIndex <= 0){
+    currentIndex += 3
+  }
+  foto.src = fotoList[currentIndex];
+}
